cb7656f56c5f0278924d25df7aa1b8413f2d2456
CarlaRuiz-ctrl <xgm558@my.utsa.edu>
2024-04-16 21:03:48 -0500

    added buttons to switch pages, productActivity still doesn't work fully

--- app/src/main/java/com/example/activiesandviews/productActivity.java
+++ app/src/main/java/com/example/activiesandviews/productActivity.java
@@ -1,4 +1,4 @@
-package com.example.activiesandviews;
+/*package com.example.activiesandviews;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -107,4 +107,4 @@ public class productActivity  extends ComponentActivity{
     }
 }
 
-
+*/
